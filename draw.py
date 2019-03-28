50b8257474a6faa44f657f2fc4d21c76f214f93e
aimran <aimran@stuycs.org>
2019-03-28 15:34:21 -0400

    Trying to fix culling

--- draw.py
+++ draw.py
@@ -3,7 +3,7 @@ from matrix import *
 
 
 def normal(ax,ay,bx,by):
-    return ax*by - ay*bx
+    return ((ax*by) - (ay*bx))
 
 def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
     add_edge(polygons,x0,y0,z0,x1,y1,z1)
@@ -14,9 +14,13 @@ def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
 def draw_polygons( polygons, screen, color ):
     i = 0
     while i < len(polygons) - 2:
-        normal = (polygons[i+1][0]-polygons[i][0], polygons[i+1][1]-polygons[i][1],
-                  polygons[i+2][0]-polygons[i][0], polygons[i+2][1]-polygons[i][1])
-        if normal > 0:
+        print(polygons[i])
+        print(polygons[i+1])
+        print(polygons[i+2])
+        print("__________________________________________________________________")
+        n = normal(polygons[i+1][0]-polygons[i][0], polygons[i+1][1]-polygons[i][1],
+                   polygons[i+2][0]-polygons[i][0], polygons[i+2][1]-polygons[i][1])
+        if n == 0:
             draw_line(int(polygons[i][0]), int(polygons[i][1]), int(polygons[i+1][0]), int(polygons[i+1][1]), screen, color)
             draw_line(int(polygons[i][0]), int(polygons[i][1]), int(polygons[i+2][0]), int(polygons[i+2][1]), screen, color)
             draw_line(int(polygons[i+2][0]), int(polygons[i+2][1]), int(polygons[i+1][0]), int(polygons[i+1][1]), screen, color)
